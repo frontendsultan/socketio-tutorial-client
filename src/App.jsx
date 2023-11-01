0cb712d748a4cfbf3d0745efbeb7b96e102b1541
Talha Rehman <talha.9197483@talmeez.pk>
2023-11-01 02:05:07 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,111 @@
+
+import React from "react";
+import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
-import useWebsocket from "react-use-websocket";
-function App() {
+// 2.Sending a connection request to server
+const socket = io.connect("http://localhost:8000");
 
-  const [sendMsg, setSendMsg] = useState("");
-  const [receivedMsg, setReceivedMsg] = useState("");
+export default function App() {
+   //Room State
+   const [room, setRoom] = useState("");
 
-  const [socketURL, setSocketURL] = useState("ws://localhost:8000");
-
-  const { sendMessage, lastMessage, readyState, getWebSocket } = useWebsocket(socketURL);
-  //2. send handshake request to server
-  useWebsocket(socketURL, {
-    // 4. if user accepts the connection
-    onOpen: () => {
-      console.log("web socket connection is established");
-    },
-    // 8.Receiving message from server
-    onMessage:(message)=>{
-      setReceivedMsg(message)
+  // 4. This function is connected to submit button. On typing the input and submitting the btn. if the room state does not exist. it will set the room value to input value and emits the custom event "join room" with the custom room id with which it wants to join
+   const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
     }
-  });
+  };
 
+  // Messages States
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
 
+  const sendMessage = () => {
+    // 6. client is sending a message to server by emitting a custom event named "send_message" with the message and room it wants to send.
+    socket.emit("send_message", { message,room });
+    console.log("msg");
+  };
 
-  function formSubmitHandler(e) {
-    e.preventDefault();
-    // 5. on submitting the form, the useWebsocket property 'sendMessage' is used to send message in the input. websocket first converts into buffer to increase efficiency
-    sendMessage(sendMsg + "I am a client");
-  }
-// 9. closing connection.
-function closeConnection(){
-  const socket = getWebSocket();
-    if (socket) {
-      socket.close();
-      console.log('socket is closed')
-    }
-}
+  // 9. Whenever the component first mounts, it check whether server sent him a message first. After that, whenever socket variable changes, it attempts to listen any event emitted by server
+  useEffect(() => {
+    socket.on("reply", (data) => {
+      setMessageReceived(data);
+      // console.log(data)
+    });
+  }, [socket]);
 
   return (
-    <>
-      <div>Received Message: {receivedMsg && receivedMsg.data}</div>
-      <div>
-        <form action="" onSubmit={(e) => formSubmitHandler(e)}>
-          <input
-            type="text"
-            value={sendMsg}
-            onChange={(e) => setSendMsg(e.target.value)}
-          />
-          <button type="submit">Send Message</button>
-        </form>
-        <button onClick={closeConnection}>Close Connection</button>
-      </div>
-    </>
+    <div className="App">
+       <input
+        placeholder="Room Number..."
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <button onClick={joinRoom}> Join Room</button>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={() => sendMessage()}> Send Message</button>
+      <h1> Message: {messageReceived && messageReceived}</h1>
+    </div>
   );
 }
 
-export default App;
+
+
+
+/*---------------------------------------
+-----------------------------------------
+A simple socket.io client example without room
+----------------------------------------
+----------------------------------------
+*/
+
+
+// import React from "react";
+// import io from "socket.io-client";
+// import { useEffect, useState } from "react";
+
+// 2.Sending a connection request to server
+// const socket = io.connect("http://localhost:8000");
+
+// export default function App() {
+//  ;
+
+//   // Messages States
+//   const [message, setMessage] = useState("");
+//   const [messageReceived, setMessageReceived] = useState("");
+
+//   const sendMessage = () => {
+//     4. client is sending a message to server by emitting an custom event named "send_message" with the message it wants to send.
+//     socket.emit("send_message", { message });
+//     console.log("msg");
+//   };
+
+//   7. Whenever the component first mounts, it check whether server sent him a message first. After that, whenever socket variable changes, it attempts to listen any event emitted by server
+//   useEffect(() => {
+//     socket.on("reply", (data) => {
+//       setMessageReceived(data);
+//       // console.log(data)
+//     });
+//   }, [socket]);
+
+//   return (
+//     <div className="App">
+      
+//       <input
+//         placeholder="Message..."
+//         onChange={(event) => {
+//           setMessage(event.target.value);
+//         }}
+//       />
+//       <button onClick={() => sendMessage()}> Send Message</button>
+//       <h1> Message: {messageReceived && messageReceived}</h1>
+//     </div>
+//   );
+// }
